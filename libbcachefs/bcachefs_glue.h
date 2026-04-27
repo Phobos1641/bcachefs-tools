3d4c7a35b456804d6f131c7be5d6d2b6f7a75b95
Lysander Sokolov <Kijinmaru@proton.me>
2026-04-27 11:29:19 +0000

    glue: compat __counted_by and TYPEOF_UNQUAL

--- libbcachefs/bcachefs_glue.h
+++ libbcachefs/bcachefs_glue.h
@@ -1,4 +1,14 @@
 #ifndef _BCACHEFS_GLUE_H
 #define _BCACHEFS_GLUE_H
 
+#include <linux/compiler.h>
+
+#ifndef __counted_by
+	#define __counted_by(nr)
+#endif
+
+#ifndef TYPEOF_UNQUAL
+	#define TYPEOF_UNQUAL(exp) __typeof_unqual__(exp)
+#endif
+
 #endif /* _BCACHEFS_GLUE_H */
